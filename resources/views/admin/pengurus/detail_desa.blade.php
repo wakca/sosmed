2501ad1bd1b523f2b6381febea656d34785f747d
antoniosai <finallyantonio@gmail.com>
2018-07-09 11:22:54 +0700

    Seting sebagai pengurus desa Issue #4

--- resources/views/admin/pengurus/detail_desa.blade.php
+++ resources/views/admin/pengurus/detail_desa.blade.php
@@ -65,13 +65,13 @@
     <div class="modal-content">
       <div class="modal-header">
         <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-        <h4 class="modal-title"><span class='glyphicon glyphicon-exclamation-sign'></span> Hapus User</h4>
+        <h4 class="modal-title"><span class='glyphicon glyphicon-exclamation-sign'></span> Set Pengurus</h4>
       </div>
       <div class="modal-body">
         <p>Anda yakin ingin menjadikan user ini sebagai Pengurus <strong>Desa {{ $desa->nama }}</strong> ?</p>
       </div>
       <div class="modal-footer">
-        <button type="button" id='btn-set-pengurus' class="btn btn-sm  btn-success"><span class='glyphicon glyphicon-trash'></span> Ya</button>
+        <button type="button" id='btn-set-pengurus' class="btn btn-sm  btn-success"><span class='glyphicon glyphicon-check'></span> Ya</button>
         <button type="button" class="btn btn-sm btn-default" data-dismiss="modal">Batal</button>
       </div>
     </div><!-- /.modal-content -->
