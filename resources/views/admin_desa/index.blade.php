9ae936a7c60d0ed77ceedc735457945350e094a7
antoniosai <finallyantonio@gmail.com>
2018-07-27 10:57:30 +0700

    Peraikan peta

--- resources/views/admin_desa/index.blade.php
+++ resources/views/admin_desa/index.blade.php
@@ -65,7 +65,7 @@
                     <p class="category">Desa {{ $desa->nama }}</p>
                 </div>
                 <div class="content" style="margin: 0px">
-                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d63327.62557958381!2d107.92223339384515!3d-7.243502211376229!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e68b20093628bcd%3A0xbf2c59113f94d3aa!2sGodog%2C+Karangpawitan%2C+Garut+Regency%2C+West+Java!5e0!3m2!1sen!2sid!4v1531465472774" width="100%" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+                    <iframe src="http://petadesa.klikdesa.com/mod/peta.php?id={{ $desa->id }}" width="100%" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
                 </div>
             </div>
         </div>
