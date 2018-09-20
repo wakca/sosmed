66438c3333e0536d9fa1b4dd4556fc0c0a467bc9
Saddam Almahali <saddam.almahali@gmail.com>
2018-09-21 03:13:49 +0700

    perbaikan meta_tag

--- app/Helpers/Getter.php
+++ app/Helpers/Getter.php
@@ -286,7 +286,7 @@ class Getter
         $images = $dom->getElementsByTagName('img');
         if(isset($images[0])){
             // return "<div class='thumbnail-story' style='background-image:url(".$images[0]->getAttribute('src').");' alt='$title' title='$title'></div>";
-            return url('/').$images[0]->getAttribute('src');
+            return $images[0]->getAttribute('src');
         } else {
             // http://d2pa5gi5n2e1an.cloudfront.net/id/images/common/no_image_l.gif
             return 'http://d2pa5gi5n2e1an.cloudfront.net/id/images/common/no_image_l.gif';
