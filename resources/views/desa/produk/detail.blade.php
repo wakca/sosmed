cf91147e3a58d4e590d903ddde2d1162f6475439
Saddam Almahali <saddam.almahali@gmail.com>
2018-10-13 05:25:09 +0700

    tambah sidebar pada produk unggulan

--- resources/views/desa/produk/detail.blade.php
+++ resources/views/desa/produk/detail.blade.php
@@ -4,7 +4,7 @@
 <div class="container">
     <div class='row'>
         <div class='col-md-9' id="story-content">
-            
+
 
             <div class='panel panel-default'>
                 <div class='panel-heading'>
@@ -16,11 +16,27 @@
                 </div>
             </div>
         </div>
-        
+
         <div class='col-md-3'>
-            {{-- @if($data == "view") --}}
-                {{-- @include('stories.random') --}}
-            {{-- @e?ndif --}}
+            @if($produk->des)
+                <div class="panel panel-primary">
+                    <div class="panel-heading">
+                        <h3 class="panel-title">Produk Unggulan {{$produk->des->nama}}</h3>
+                    </div>
+                    <div class="panel-body">
+                        @if($produk->des->produk_unggulan()->count() > 1)
+                        <ul>
+                            @foreach($produk->des->produk_unggulan()->where('id', '!=', $produk->id)->limit(5)->get() as $prodes)
+                                <li>{{$prodes->nama}}</li>
+                            @endforeach
+                        </ul>
+
+                        @else
+                            <center><span class="text-muted">Tidak Terdapat Produk Unggulan lainnya</span></center>
+                        @endif
+                    </div>
+                </div>
+            @endif
         </div>
     </div>
 </div>
